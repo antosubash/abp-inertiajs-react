45c93225bdc9bffb0974377c5b12b05c9614fc40
Anto Subash <antosubash@live.com>
2024-10-07 22:02:30 +0200

    refactor: Update Main method signature in Program.cs
    - Changed the signature of the Main method in the Program class from "public async static Task<int> Main(string[] args)" to "public static async Task<int> Main(string[] args)"
    - This change improves the readability and consistency of the code

--- InertiaDemo/InertiaDemo/Program.cs
+++ InertiaDemo/InertiaDemo/Program.cs
@@ -9,7 +9,7 @@ namespace InertiaDemo;
 
 public class Program
 {
-    public async static Task<int> Main(string[] args)
+    public static async Task<int> Main(string[] args)
     {
         try
         {
@@ -25,45 +25,54 @@ public class Program
                 options.ManifestFilename = "manifest.json";
             });
 
-
-
-            builder.Host.AddAppSettingsSecretsJson()
+            builder
+                .Host.AddAppSettingsSecretsJson()
                 .UseAutofac()
-                .UseSerilog((context, services, loggerConfiguration) =>
-                {
-                    loggerConfiguration
+                .UseSerilog(
+                    (context, services, loggerConfiguration) =>
+                    {
+                        loggerConfiguration
 #if DEBUG
-                        .MinimumLevel.Debug()
+                            .MinimumLevel.Debug()
 #else
-            .MinimumLevel.Information()
+                            .MinimumLevel.Information()
 #endif
-                        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-                        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
-                        .Enrich.FromLogContext()
-                        .WriteTo.Async(c => c.File("Logs/logs.txt"))
-                        .WriteTo.Async(c => c.Console());
-                    
-                    if (IsMigrateDatabase(args))
-                    {
-                        loggerConfiguration.MinimumLevel.Override("Volo.Abp", LogEventLevel.Warning);
-                        loggerConfiguration.MinimumLevel.Override("Microsoft", LogEventLevel.Warning);
+                            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                            .MinimumLevel.Override(
+                                "Microsoft.EntityFrameworkCore",
+                                LogEventLevel.Warning
+                            )
+                            .Enrich.FromLogContext()
+                            .WriteTo.Async(c => c.File("Logs/logs.txt"))
+                            .WriteTo.Async(c => c.Console());
+
+                        if (IsMigrateDatabase(args))
+                        {
+                            loggerConfiguration.MinimumLevel.Override(
+                                "Volo.Abp",
+                                LogEventLevel.Warning
+                            );
+                            loggerConfiguration.MinimumLevel.Override(
+                                "Microsoft",
+                                LogEventLevel.Warning
+                            );
+                        }
+                        else
+                        {
+                            loggerConfiguration.WriteTo.Async(c => c.AbpStudio(services));
+                        }
                     }
-                    else
-                    {
-                        loggerConfiguration.WriteTo.Async(c => c.AbpStudio(services));
-                    }
-                });
-            if (IsMigrateDatabase(args))
-            {
-                builder.Services.AddDataMigrationEnvironment();
-            }
+                );
+            builder.Services.AddDataMigrationEnvironment();
             await builder.AddApplicationAsync<InertiaDemoModule>();
             var app = builder.Build();
             await app.InitializeApplicationAsync();
 
             if (IsMigrateDatabase(args))
             {
-                await app.Services.GetRequiredService<InertiaDemoDbMigrationService>().MigrateAsync();
+                await app
+                    .Services.GetRequiredService<InertiaDemoDbMigrationService>()
+                    .MigrateAsync();
                 return 0;
             }
 
